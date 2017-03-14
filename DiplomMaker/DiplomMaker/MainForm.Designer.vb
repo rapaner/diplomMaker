acf0c94dc407479dbd1f88226097f299d314be01
rapaner <rapaner@yandex.ru>
2017-03-14 23:33:32 +0300

    Новая концепция

--- DiplomMaker/DiplomMaker/MainForm.Designer.vb
+++ DiplomMaker/DiplomMaker/MainForm.Designer.vb
@@ -23,48 +23,30 @@ Partial Class MainForm
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
-        Me.ФайлToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.СверитьБазуToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.СформироватьГрамотыToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.СформироватьДипломыToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.НастройкаToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.СправкаToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.ВыходToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.btnEnterResults = New System.Windows.Forms.Button()
+        Me.btnExportResults = New System.Windows.Forms.Button()
+        Me.btnEditDB = New System.Windows.Forms.Button()
+        Me.btnReviewResults = New System.Windows.Forms.Button()
         Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
         'MenuStrip1
         '
-        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ФайлToolStripMenuItem, Me.СправкаToolStripMenuItem, Me.ВыходToolStripMenuItem})
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.НастройкаToolStripMenuItem, Me.СправкаToolStripMenuItem, Me.ВыходToolStripMenuItem})
         Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
         Me.MenuStrip1.Name = "MenuStrip1"
         Me.MenuStrip1.Size = New System.Drawing.Size(536, 24)
         Me.MenuStrip1.TabIndex = 0
         Me.MenuStrip1.Text = "MenuStrip1"
         '
-        'ФайлToolStripMenuItem
+        'НастройкаToolStripMenuItem
         '
-        Me.ФайлToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.СверитьБазуToolStripMenuItem, Me.СформироватьГрамотыToolStripMenuItem, Me.СформироватьДипломыToolStripMenuItem})
-        Me.ФайлToolStripMenuItem.Name = "ФайлToolStripMenuItem"
-        Me.ФайлToolStripMenuItem.Size = New System.Drawing.Size(48, 20)
-        Me.ФайлToolStripMenuItem.Text = "Файл"
-        '
-        'СверитьБазуToolStripMenuItem
-        '
-        Me.СверитьБазуToolStripMenuItem.Name = "СверитьБазуToolStripMenuItem"
-        Me.СверитьБазуToolStripMenuItem.Size = New System.Drawing.Size(213, 22)
-        Me.СверитьБазуToolStripMenuItem.Text = "Сверить базу"
-        '
-        'СформироватьГрамотыToolStripMenuItem
-        '
-        Me.СформироватьГрамотыToolStripMenuItem.Name = "СформироватьГрамотыToolStripMenuItem"
-        Me.СформироватьГрамотыToolStripMenuItem.Size = New System.Drawing.Size(213, 22)
-        Me.СформироватьГрамотыToolStripMenuItem.Text = "Сформировать грамоты"
-        '
-        'СформироватьДипломыToolStripMenuItem
-        '
-        Me.СформироватьДипломыToolStripMenuItem.Name = "СформироватьДипломыToolStripMenuItem"
-        Me.СформироватьДипломыToolStripMenuItem.Size = New System.Drawing.Size(213, 22)
-        Me.СформироватьДипломыToolStripMenuItem.Text = "Сформировать дипломы"
+        Me.НастройкаToolStripMenuItem.Name = "НастройкаToolStripMenuItem"
+        Me.НастройкаToolStripMenuItem.Size = New System.Drawing.Size(78, 20)
+        Me.НастройкаToolStripMenuItem.Text = "Настройка"
         '
         'СправкаToolStripMenuItem
         '
@@ -78,15 +60,55 @@ Partial Class MainForm
         Me.ВыходToolStripMenuItem.Size = New System.Drawing.Size(53, 20)
         Me.ВыходToolStripMenuItem.Text = "Выход"
         '
+        'btnEnterResults
+        '
+        Me.btnEnterResults.Location = New System.Drawing.Point(165, 47)
+        Me.btnEnterResults.Name = "btnEnterResults"
+        Me.btnEnterResults.Size = New System.Drawing.Size(194, 56)
+        Me.btnEnterResults.TabIndex = 1
+        Me.btnEnterResults.Text = "Ввести результаты"
+        Me.btnEnterResults.UseVisualStyleBackColor = True
+        '
+        'btnExportResults
+        '
+        Me.btnExportResults.Location = New System.Drawing.Point(165, 171)
+        Me.btnExportResults.Name = "btnExportResults"
+        Me.btnExportResults.Size = New System.Drawing.Size(194, 56)
+        Me.btnExportResults.TabIndex = 2
+        Me.btnExportResults.Text = "Экспорт в xlsx"
+        Me.btnExportResults.UseVisualStyleBackColor = True
+        '
+        'btnEditDB
+        '
+        Me.btnEditDB.Location = New System.Drawing.Point(165, 233)
+        Me.btnEditDB.Name = "btnEditDB"
+        Me.btnEditDB.Size = New System.Drawing.Size(194, 56)
+        Me.btnEditDB.TabIndex = 3
+        Me.btnEditDB.Text = "Редактировать базы данных"
+        Me.btnEditDB.UseVisualStyleBackColor = True
+        '
+        'btnReviewResults
+        '
+        Me.btnReviewResults.Location = New System.Drawing.Point(165, 109)
+        Me.btnReviewResults.Name = "btnReviewResults"
+        Me.btnReviewResults.Size = New System.Drawing.Size(194, 56)
+        Me.btnReviewResults.TabIndex = 4
+        Me.btnReviewResults.Text = "Просмотр результатов"
+        Me.btnReviewResults.UseVisualStyleBackColor = True
+        '
         'MainForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(536, 344)
+        Me.ClientSize = New System.Drawing.Size(536, 314)
+        Me.Controls.Add(Me.btnReviewResults)
+        Me.Controls.Add(Me.btnEditDB)
+        Me.Controls.Add(Me.btnExportResults)
+        Me.Controls.Add(Me.btnEnterResults)
         Me.Controls.Add(Me.MenuStrip1)
         Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "MainForm"
-        Me.Text = "DiplomMaker"
+        Me.Text = "DiplomaMaker"
         Me.MenuStrip1.ResumeLayout(False)
         Me.MenuStrip1.PerformLayout()
         Me.ResumeLayout(False)
@@ -94,11 +116,11 @@ Partial Class MainForm
 
     End Sub
     Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
-    Friend WithEvents ФайлToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
-    Friend WithEvents СверитьБазуToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
-    Friend WithEvents СформироватьГрамотыToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
-    Friend WithEvents СформироватьДипломыToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents НастройкаToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
     Friend WithEvents СправкаToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
-    Friend WithEvents ВыходToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
-
+    Friend WithEvents btnEnterResults As Button
+    Friend WithEvents ВыходToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents btnExportResults As Button
+    Friend WithEvents btnEditDB As Button
+    Friend WithEvents btnReviewResults As Button
 End Class
--- DiplomMaker/DiplomMaker/MainForm.vb
+++ DiplomMaker/DiplomMaker/MainForm.vb
@@ -1,12 +1,14 @@
 ﻿Public Class MainForm
 
+    Private Sub СверитьБазуToolStripMenuItem_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub BtnEnterResults_Click(sender As Object, e As EventArgs) Handles btnEnterResults.Click
+
+    End Sub
+
     Private Sub ВыходToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ВыходToolStripMenuItem.Click
         Application.Exit()
     End Sub
-
-    Private Sub СверитьБазуToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles СверитьБазуToolStripMenuItem.Click
-        Dim checkBase As New CheckBase
-        checkBase.ShowDialog()
-    End Sub
-
 End Class
